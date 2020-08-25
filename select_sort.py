55eea5ad513225194944449e5b7f607f4f480540
lrddrl <ruodongl@gmail.com>
2020-08-25 07:31:51 -0400

    sort

--- select_sort.py
+++ select_sort.py
@@ -1,15 +1,25 @@
 def select_sort_simple(li):
     li_new = []
-    for i in range(len(li)):
-        min_val = min(li)
-        li_new.append(min_val)
+    for i in range(len(li)): #时间复杂度 O(N)
+        min_val = min(li)       #时间复杂度 O(N)
+        li_new.append(min_val)  
         li.remove(min_val)
     return li_new
-
+ #时间复杂度 O(N^2)
 
 def select_sort(li):
-    for i in range(len(li)-1):
+    for i in range(len(li)-1): # i是第几趟
+        min_loc = i
+        for j in range(i,len(li)):
+            if li[j] < li[min_loc]:
+                min_loc = j
+        li[i], li[min_loc] = li[min_loc] , li[i]
+        
+        print(li)
+
         
 
 li = [3, 4, 5, 1, 6, 7, 9, 8]
-print(select_sort_simple(li))
+#print(select_sort_simple(li))
+#print(li)
+select_sort(li)
